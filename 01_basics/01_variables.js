d63df068dd76d48d3f26196edbd24b9dc7cbc5c8
Shlok Singh <165383464+ShlokSig@users.noreply.github.com>
2024-04-10 08:03:26 +0000

    test

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,6 @@ let accountEmail = "shlok@gmail.com"
 var accountPassword = "1234"
 accountCity = "Mumbai"
 
-// accountId = "767654" // not allowed
+accountId = "767654"
 
-console.log("accountId");
+console.log(accountId);
